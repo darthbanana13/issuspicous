e7b48ad8d6e21608336b8d117b73f3c35fd7dff0
Radu Margineanu <radu.margineanu@scayla.com>
2021-01-03 13:44:26 +0100

    Add only TLS 1.3 Ciphers

--- pkg/certParser/certParser.go
+++ pkg/certParser/certParser.go
@@ -10,6 +10,14 @@ import (
 const defaultTimeoutSeconds = 3
 const insecureSkipVerify = false
 
+// Only TLS 1.3 Cipers for security reasons
+// To add older TLS Ciphers for compatibility reasons consult https://wiki.mozilla.org/Security/Server_Side_TLS
+var cipherSuites = []uint16{
+	tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_CHACHA20_POLY1305_SHA256,
+}
+
 func Get(site, port string) {
 	d := &net.Dialer{
 			Timeout: time.Duration(defaultTimeoutSeconds) * time.Second,
@@ -17,6 +25,7 @@ func Get(site, port string) {
 
 	conn, _ := tls.DialWithDialer(d, "tcp", site + ":" + port, &tls.Config{
 		InsecureSkipVerify: insecureSkipVerify,
+		CipherSuites:       cipherSuites,
 		MaxVersion:         tls.VersionTLS13,
 	})
 	defer conn.Close()
